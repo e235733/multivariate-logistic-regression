a2a9687f3350eb6ef4e1436985d26cfc42eb2820
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-02-18 06:05:11 +0900

    feat: sample generator

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello");
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+
+N_SAMPLES = 100
+N_FEATURES = 2
+N_CENTERS = 3
+CLUSTER_STD = 10
+CLUSTER_RANGE = 100
+
+
+X, Y = make_blobs(n_samples=N_SAMPLES, n_features=N_FEATURES, centers=N_CENTERS, cluster_std=CLUSTER_STD, center_box=(-CLUSTER_RANGE, CLUSTER_RANGE))
+print(X, Y)
+
+plt.figure(figsize=(6, 6))
+plt.title("generated data")
+plt.scatter(X[:, 0], X[:, 1], marker='o', c=Y, s=25, edgecolor='k')
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
+plt.show()
